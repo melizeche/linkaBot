ba81d2167ba319341de09548f7d8a3c30ab4c831
Marcelo Elizeche Landó <melizeche@gmail.com>
2024-11-30 05:03:44 -0300

    Add geolocation override support, smaller image

--- screen.py
+++ screen.py
@@ -29,13 +29,28 @@ def get_screenshot():
     try:
         with webdriver.Chrome(options=options) as driver:
             driver.set_window_size(900, 900)
+            driver.execute_cdp_cmd(
+                "Browser.grantPermissions",
+                {
+                    "origin": "https://airelib.re/",
+                    "permissions": ["geolocation"],
+                },
+            )
+            driver.execute_cdp_cmd(
+                "Emulation.setGeolocationOverride",
+                {
+                    "latitude": -25.250,
+                    "longitude": -57.536,
+                    "accuracy": 100,
+                },
+            )
             driver.get(URL)
             time.sleep(6)
             driver.save_screenshot(screenshot_path._str)
 
         im = Image.open(screenshot_path)
         # Set box coordinates to crop (in pixels)
-        left, top, right, bottom = 200, 170, 700, 600
+        left, top, right, bottom = 200, 170, 700, 550
         cropped_img = im.crop((left, top, right, bottom))
         cropped_img.save(screenshot_path)
     except Exception as e:
